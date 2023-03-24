07e0b14748c938a5bfb080e87746bb125c85ae95
Marshal Horn <kamocat@gmail.com>
2023-03-24 08:52:18 -0700

    Fixed sign bit extension, added pretty formatting

--- src/adc_test.py
+++ src/adc_test.py
@@ -1,13 +1,20 @@
 from mcp3428 import ADC
 import asyncio
 
+def print_with_units(volts):
+    scale = [1e5,1e5,1e5,11]
+    units = ['mA','mA','mA','V']
+    vals = ["{:.2f}".format(scale[i]*volts[i])+units[i] for i in range(4)]
+    print(vals)
+
+
 async def main():
     adc = ADC()
     gain = [8,8,8,1]
     print("Initialized")
     while True:
         volts = await adc.get_all(gain)
-        print(volts)
+        print_with_units(volts)
         await asyncio.sleep(1)
 
 task = asyncio.run(main())
--- src/mcp3428.py
+++ src/mcp3428.py
@@ -3,6 +3,10 @@ import asyncio
 
 lock = asyncio.Lock()
 
+def sign_extend(value, bits):
+    sign_bit = 1 << (bits - 1)
+    return (value & (sign_bit - 1)) - (value & sign_bit)
+
 class ADC:
     def __init__(self,address=0x68):
         self.addr = address
@@ -23,7 +27,7 @@ class ADC:
         cfg = (channel << 5) | gain | 0x88
         async with lock:
             self.bus.write_byte(self.addr, cfg)
-            await asyncio.sleep(1/15) 
+            await asyncio.sleep(0.1) 
             #SMBus requires that we write to a register
             #so we re-write the config byte without restarting the conversion
             data = self.bus.read_i2c_block_data(self.addr,cfg&0x7f,3)
@@ -33,6 +37,8 @@ class ADC:
         #Convert to volts
         scale = (0.5 ** gain) * 0.001/16
         volts = data[0]<<8 | data[1] 
+        #volts = hex(volts)
+        volts = sign_extend(volts, 16)
         volts *= scale
         return volts
 
