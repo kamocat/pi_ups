b35fd2b7657128bf77656b916ff2b39e050bafbf
Marshal Horn <kamocat@gmail.com>
2023-03-26 19:54:15 +0100

    Logging voltage and current to sqlite db

--- src/adc_test.py
+++ src/adc_test.py
@@ -1,5 +1,11 @@
+import time
+import sqlite3
 from mcp3428 import ADC
 import asyncio
+
+con = sqlite3.connect("battery.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS ups(time INTEGER, input1 REAL, input2 REAL, consumption REAL, volts REAL);")
 
 def print_with_units(volts):
     scale = [1e5,1e5,1e5,11]
@@ -9,14 +15,22 @@ def print_with_units(volts):
     vals = [units[i].format(scale[i]*volts[i]) for i in range(4)]
     print(vals)
 
-
 async def main():
     adc = ADC()
     gain = [8,8,8,1]
     print("Initialized")
+    mA = 1e5
+    volts = 11
     while True:
-        volts = await adc.get_all(gain)
-        print_with_units(volts)
+        vals = await adc.get_all(gain)
+        data = (int(time.time()), 
+            vals[0]*mA, 
+            vals[1]*mA, 
+            vals[2]*mA, 
+            vals[3]*volts)
+        cur.execute("INSERT INTO ups VALUES (?,?,?,?,?)", data)
+        con.commit()
+        print_with_units(vals)
         await asyncio.sleep(1)
 
 task = asyncio.run(main())
